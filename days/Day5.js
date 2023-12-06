3e726589b9082edea56318f2dd510617caec9e0b
Helcio Itiyama <helcio.itiyama-ext@dentsu.com>
2023-12-06 12:17:17 -0300

    aoc Day 05 part 02

--- days/Day5.js
+++ days/Day5.js
@@ -60,3 +60,109 @@ const dayFivePart1 =  () => {
 
 console.log(`Day 05 part 01 result is: ${dayFivePart1()}`)
 
+
+const getNextRange = (arr, seedRange) => {
+  const nextDestRange = [];
+  const [sources, destinations] = arr.reduce((acc, each) => {
+    const min = Number(each[1]);
+    const max = Number(each[1]) + Number(each[2]) - 1;
+    const dest = Number(each[0]);
+    const maxDest = Number(each[0]) + Number(each[2]) - 1;
+    acc[0].push([min, max]) 
+    acc[1].push([dest, maxDest])
+    return acc;
+  },[[],[]])
+
+  for(let i = 0; i < seedRange.length; i++) {
+    const destRange = [];
+    let minSeed = seedRange[i][0];
+    let maxSeed = seedRange[i][1];
+
+    for(let s = 0; s < sources.length; s++) {
+      const min = sources[s][0];
+      const max = sources[s][1];
+      if(minSeed < min) {
+        destRange.push([minSeed, min - 1])
+      }
+
+      if(minSeed >= min && maxSeed <= max) {
+        const dest = minSeed - min + destinations[s][0];
+        destRange.push([dest, destinations[s][1]])
+        minSeed = maxSeed + 1;
+        if(minSeed > maxSeed) break;
+      }
+
+      if(max >= minSeed && maxSeed > max) {
+        const dest = minSeed - min + destinations[s][0];
+        destRange.push([dest, destinations[s][1]])
+        minSeed = max + 1;
+        if(minSeed > maxSeed) break;
+      }
+    }
+
+    if(minSeed > sources[sources.length - 1][1] && maxSeed > sources[sources.length - 1][1]) {
+      destRange.push([minSeed, maxSeed])
+    }
+
+    if(minSeed <= sources[sources.length - 1][1] && maxSeed > sources[sources.length - 1][1]) {
+      destRange.push([sources[sources.length - 1][1], maxSeed])
+    }
+    nextDestRange.push(...destRange)
+  }
+
+  return nextDestRange;
+}
+
+const dayFivePart2 =  () => { 
+  const locationMap = getInput("input_day5"); 
+  const categories = getCategories(locationMap)
+  const seeds = categories[0].map(data => data.split(' '))[0];
+  const lowestLocation = []
+  let arr = [];
+
+  const seedArray = seeds.reduce((t, e, i)=> {
+    if(i%2 !== 0) {
+      arr.push(e);
+      t.push(arr);
+      arr = []
+    } else {
+      arr.push(e);
+    }
+    return t;
+  }, [])
+
+  const sections = categories.reduce((acc, section, i )=> {
+    if(i > 0) {
+      acc.push(section.map(data => data.split(' ')));
+    } 
+    return acc;
+  }, [])
+
+  const sortedSections = sections.map(section => section.sort((a, b) => Number(a[1]) - Number(b[1])));
+
+  for (const each of seedArray) {
+      let seed = []
+      seed.push(each.reduce((t, e , i)=> {
+        if(i === 0) {
+          t.push(Number(e));
+        } else {
+          t.push(t[i-1] + Number(e) - 1);
+        }
+        return t;
+      }, []))
+
+      for(let i = 0; i < sortedSections.length; i++) {
+        seed = getNextRange(sortedSections[i], seed);
+      }
+      lowestLocation.push(...seed);
+  }
+
+  return lowestLocation.reduce( (t, e)=> {
+    if(e[0] !== 0 && t > e[0]) {
+      t = e[0];
+    }
+    return t;
+  }, Number.MAX_VALUE);
+}
+
+console.log(`Day 05 part 02 result is: ${dayFivePart2()}`)
